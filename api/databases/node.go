ad9aa583da5b1b3df1c7603a0cbf4f6c690c230f
jiazhi.xue <xuejiazhi@gmail.com>
2024-01-14 20:51:53 +0800

    Update node.go

--- api/databases/node.go
+++ api/databases/node.go
@@ -7,7 +7,7 @@ type TNodePoint struct {
 	NodeName string `gorm:"column(node_name);size(64);unique"`
 	NodeUrl  string `gorm:"column(node_url);size(264)"`
 	NodeIp   string `gorm:"column(node_ip);size(264)"`
-	Version  string `gorm:"column(node_ip);size(65)"`
+	Version  string `gorm:"column(version);size(65)"`
 }
 
 func AddNode(nodeInfo *TNodePoint) error {
